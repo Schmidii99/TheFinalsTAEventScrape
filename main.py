710c00f87c5be455291e6984b7c06125008892f1
Florian Schmid <70525898+Schmidii99@users.noreply.github.com>
2024-05-16 21:35:22 +0200

    write data to db

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import database
+import requests
+import sqlite3
 
+
+URL = "https://storage.googleapis.com/embark-discovery-leaderboard/community-event-2-8-leaderboard-discovery-live.json"
 
 if __name__ == '__main__':
-    database.init_db()
+    conn = sqlite3.connect(database.DB_NAME)
+    c = conn.cursor()
+
+    database.init_db(c)
+    conn.commit()
+
+    response = requests.get(URL).json()
+    database.add_scrape(c, response["goal"], response["total"])
+    conn.commit()
+
+    for entry in response["entries"]:
+        if not database.does_user_exist(c, entry["name"]):
+            database.add_user(c, entry["name"], entry["steam"], entry["xbox"], entry["psn"])
+            conn.commit()
+        database.add_connection(c, entry["name"], entry["r"], entry["k"])
+        conn.commit()
+
+    conn.close()
