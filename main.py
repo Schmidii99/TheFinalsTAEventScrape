69f495a10f68a488916c9d3022f01158db71af8a
Florian Schmid <70525898+Schmidii99@users.noreply.github.com>
2024-05-17 10:32:15 +0200

    split save to db and save as json

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import database
 import requests
 import sqlite3
-
+import os
+import datetime
 
 URL = "https://storage.googleapis.com/embark-discovery-leaderboard/community-event-2-8-leaderboard-discovery-live.json"
 
-if __name__ == '__main__':
+
+def save_json_file():
+    response = requests.get(URL)
+
+    if not os.path.exists("./data"):
+        os.makedirs("./data")
+
+    with open("./data/" + str(datetime.datetime.now()).replace(" ", "_").replace(":", "_") + ".json", "wb") as f:
+        f.write(response.content)
+
+
+def save_in_db():
     conn = sqlite3.connect(database.DB_NAME)
     c = conn.cursor()
 
@@ -24,3 +36,7 @@ if __name__ == '__main__':
         conn.commit()
 
     conn.close()
+
+
+if __name__ == '__main__':
+    save_in_db()
