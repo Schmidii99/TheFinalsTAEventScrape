f16007460200e381ff87238a102538ae9790090e
Florian Schmid <melonencool@gmail.com>
2024-05-19 00:08:37 +0200

    add json parsing for files in /data/ folder

--- data_processor/src/main.rs
+++ data_processor/src/main.rs
@@ -1,15 +1,49 @@
 mod database;
 use database::Database;
 
+use std::fs;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Debug, Deserialize)]
+struct Entry {
+    r: i32, // rank
+    name: String,
+    k: i32, // kills
+    steam: String,
+    xbox: String,
+    psn: String
+}
+
+
+#[derive(Debug, Deserialize)]
+struct Dataset {
+    goal: i32,
+    total: i32,
+    entries: Vec<Entry>,
+}
+
+fn read_dataset_from_file<P: AsRef<Path>>(path: P) -> Result<Dataset, Box<dyn Error>>{
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `Dataset`.
+    let u: Dataset = serde_json::from_reader(reader)?;
+
+    // Return the `Dataset`.
+    Ok(u)
+}
+
 fn main() {
-    let db: Database = Database::initialize();
-    db.add_scrape("myTime", 7000000, 1345678);
-    if db.does_user_exist("testuser") {
-        println!("testuser exists")
+    // let db: Database = Database::initialize();
+    let paths:fs::ReadDir  = fs::read_dir("./data/").unwrap();
+
+    for path in paths {
+        let res: Dataset = read_dataset_from_file(path.unwrap().path()).unwrap();
+        println!("{}", res.total)
     }
-    if !db.does_user_exist("testuser2") {
-        println!("testuser2 does not exists")
-    }
-    db.add_user("name", "steam", "xbox", "psn");
-    db.add_connection("testuser", 100, 876);
 }
